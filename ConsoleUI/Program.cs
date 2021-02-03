9577a1ea8b11fe86af1758544527387d7bb4351f
erenutk <erenutkukrts@gmail.com>
2021-02-03 23:48:39 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            //InMemoryProductDal inMemoryProductDal = new InMemoryProductDal();
+            //inMemoryProductDal.Delete(new Product { ProductId = 1 });
+
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityReposityory.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class: referans tip
+    //IEntity: IEntity veya onun implementasyonları olmalı
+    //new() : newlenebilir olmalı (IEntity olamaz)
+    public interface IEntityReposityory<T> where T:class,IEntity,new() //class referans tip olmalı  anlamında
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null); //filter=null filtre vermeyebilirsin demek
+        T Get(Expression<Func<T, bool>> filter); // filtre vermek zorunlu
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
